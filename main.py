3c36c5e4609561ca66df6c66449392570835c463
Altheir <cchancey121695@gmail.com>
2019-11-10 13:59:30 -0500

    added several tests for the check contracts function

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def check_contracts(contract):
     variable definition must be defined.
     conditions must be callable objects.
     """
-    def decorator_v(funky):
+    def decorator_v(function):
         def handle(*args, **kwargs):
-            params = inspect.signature(funky).bind(*args, **kwargs)
+            params = inspect.signature(function).bind(*args, **kwargs)
             params.apply_defaults()
             params = params.arguments
             for var, conditions in contract.items():
@@ -30,29 +30,8 @@ def check_contracts(contract):
                 else:
                     if not conditions(params[var]):
                         handle_invalid_contract(var=var, value=params[var], conditions=conditions)
-            return funky(*args, **kwargs)
+            return function(*args, **kwargs)
 
         return handle
 
     return decorator_v
-
-# check positional
-# check positional with keyword calls
-# check positional with multi-args
-# check positional with keyword calls and multi-contract
-#
-# check positional and keywords
-# check positional and keywords with keyword calls
-# check positional and keywords with multi-args
-# check positional and keywords with keyword calls and multi-contract
-#
-# check with *args
-# check with **kwargs
-# check with user types
-# check with base types
-# check with python functions
-# check with partials
-#
-# check runtime with large arrays / numpy arrays
-# check runtime for small args
-# check runtime for strings
--- tests.py
+++ tests.py
@@ -2,20 +2,180 @@ import pytest
 from main import check_contracts
 
 
+@check_contracts({'a': [lambda x: x is not None,
+                        lambda x: x > 5],
+                  'b': [lambda x: isinstance(x, str)]})
 def positional(a, b):
     print(a, b)
 
+@check_contracts({'a': lambda x: x is not None,
+                  'b': lambda x: isinstance(x, str)})
+def positional_single_restriction(a, b):
+    print(a, b)
 
+
+
+def test_positional_expected():
+    positional(7, 'foo')
+
+
+def test_positional_breaking_one_condition():
+    with pytest.raises(Exception):
+        positional(0, 'foo')
+    with pytest.raises(Exception):
+        positional(None, 'foo')
+    with pytest.raises(Exception):
+        positional(7, 11)
+
+
+def test_positional_breaking_both_conditions():
+    with pytest.raises(Exception):
+        positional(0, 1)
+
+
+def test_positional_single_expected():
+    positional_single_restriction(7, 'foo')
+
+
+def test_positional_single_breaking_one_condition():
+    with pytest.raises(Exception):
+        positional_single_restriction(None, 'foo')
+    with pytest.raises(Exception):
+        positional_single_restriction(7, 11)
+
+
+def test_positional_single_breaking_both_conditions():
+    with pytest.raises(Exception):
+        positional_single_restriction(None, 1)
+
+
+def test_positional_expected_kwarg_calls():
+    positional(a=7, b='foo')
+
+
+def test_positional_breaking_one_condition_kwarg_calls():
+    with pytest.raises(Exception):
+        positional(a=0, b='foo')
+    with pytest.raises(Exception):
+        positional(a=None, b='foo')
+    with pytest.raises(Exception):
+        positional(a=7, b=11)
+
+
+def test_positional_breaking_both_conditions_kwarg_calls():
+    with pytest.raises(Exception):
+        positional(a=0, b=1)
+
+
+def test_positional_single_expected_kwarg_calls():
+    positional_single_restriction(a=7, b='foo')
+
+
+def test_positional_single_breaking_one_condition_kwarg_calls():
+    with pytest.raises(Exception):
+        positional_single_restriction(a=None, b='foo')
+    with pytest.raises(Exception):
+        positional_single_restriction(a=7, b=11)
+
+
+def test_positional_single_breaking_both_conditions_kwarg_calls():
+    with pytest.raises(Exception):
+        positional_single_restriction(a=None, b=1)
+
+
+def test_positional_expected_mixed_calls():
+    positional(7, b='foo')
+
+
+def test_positional_breaking_one_condition_mixed_calls():
+    with pytest.raises(Exception):
+        positional(0, b='foo')
+    with pytest.raises(Exception):
+        positional(None, b='foo')
+    with pytest.raises(Exception):
+        positional(7, b=11)
+
+
+def test_positional_breaking_both_conditions_mixed_calls():
+    with pytest.raises(Exception):
+        positional(0, b=1)
+
+
+def test_positional_single_expected_mixed_calls():
+    positional_single_restriction(7, b='foo')
+
+
+def test_positional_single_breaking_one_condition_mixed_calls():
+    with pytest.raises(Exception):
+        positional_single_restriction(None, b='foo')
+    with pytest.raises(Exception):
+        positional_single_restriction(7, b=11)
+
+
+def test_positional_single_breaking_both_conditions_mixed_calls():
+    with pytest.raises(Exception):
+        positional_single_restriction(None, b=1)
+
+@check_contracts({'a': [lambda x: x is not None,
+                        lambda x: x > 5],
+                  'b': lambda x: isinstance(x, str),
+                  'args': lambda x: x is not None})
 def positional_with_variable(a, b, *args):
     print(a, b, args)
 
+@check_contracts({'args': lambda x: all(item > 5 for item in x)})
+def positional_arg_value_query(a, b, *args):
+    print(a, b, args)
 
+
+def test_positional_with_variable_args_success():
+    positional_with_variable(10, 'foo', (3, 4, 5, 6, 7))
+
+
+def test_positional_with_variable_args_success_two():
+    positional_with_variable(10, 'foo', 3, 4, 5, 6, 7)
+
+
+def test_positional_with_variable_args_failure():
+    positional_arg_value_query(10, 'foo', 10, 8, 9, 6, 7)
+    with pytest.raises(Exception):
+        positional_arg_value_query(10, 'foo', 3, 4, 5, 6, 7)
+
+
+@check_contracts({'args': lambda x: not x ,
+                  'c': lambda x: x > 5})
 def positional_and_keywords(a, b, *args, c):
     print(a, b, args, c)
 
 
+def test_positional_and_keywords_sucess():
+    positional_and_keywords(1, 2, c=7)
+
+
+def test_positional_and_keywords_failure():
+    with pytest.raises(Exception):
+        positional_and_keywords(1, 2, 3, c=7)
+
+
+def test_positional_and_keywords_failure_two():
+    with pytest.raises(Exception):
+        positional_and_keywords(1, 2, c=2)
+
+
+def test_positional_and_keywords_sucess_kwargs():
+    positional_and_keywords(a=1, b=2, c=7)
+
+
+def test_positional_and_keywords_failure_two():
+    with pytest.raises(Exception):
+        positional_and_keywords(a=1, b=2, c=2)
+
+
 def positional_with_kwargs(a, b, *args, c, **kwargs):
     print(a, b, args, c, kwargs)
+
+
+
 
 
 def positional_with_only_kwargs(a, b, **kwargs):
@@ -38,17 +198,6 @@ class UserT:
 
 
 
-# check positional
-# check positional with keyword calls
-# check positional with multi-args
-# check positional with keyword calls and multi-contract
-#
-# check positional and keywords
-# check positional and keywords with keyword calls
-# check positional and keywords with multi-args
-# check positional and keywords with keyword calls and multi-contract
-#
-# check with *args
 # check with **kwargs
 # check with user types
 # check with base types
